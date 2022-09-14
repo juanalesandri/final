7cdc8548a1ac9cd29d86b3ea3f412f43da40ad0c
Juan <juan.alesandri.acc@gmail.com>
2022-09-14 11:14:14 -0300

    Se agrega funcionalidad de supermercados

---
+++ src/app/supermarkets/supermarket-products/supermarket-products.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Supermarket } from '../supermarket.model';
+import { SupermarketService } from '../supermarket.service';
+//import { Recipe } from '../../recipe.model';
+
+@Component({
+    selector: 'app-supermarket-products',
+    templateUrl: './supermarket-products.component.html',
+    styleUrls: ['./supermarket-products.component.css']
+})
+export class SupermarketProductsComponent implements OnInit, OnDestroy {
+    id: number;
+    supermarket: Supermarket;
+    superName: string = '';
+    supermarketSubscriber: Subscription;
+    isLoading: boolean = false;
+
+    constructor(private route: ActivatedRoute, private supermarketService: SupermarketService) { }
+
+    ngOnInit(): void {
+        this.route.params.subscribe((params: Params) => {
+            this.isLoading = true;
+            this.id = parseInt(params.id);
+            this.supermarketService.getSupermarketById(this.id).subscribe((supermarket: Supermarket) => {
+                this.isLoading = false;
+                this.supermarket = supermarket;
+                console.log(this.supermarket.products);
+                this.superName = supermarket.name;
+            })
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.supermarketSubscriber.unsubscribe();
+    }
+
+}
