178865af6f99909bb86dc11677154f2bbe3fb419
Juan <juan.alesandri.acc@gmail.com>
2022-09-19 15:43:51 -0300

    Modificacion en shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.modules';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/authModule';
 import { AuthTestComponent } from './auth/auth-test.component';
 
 @NgModule({
